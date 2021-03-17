b4e022a90df1279d500cf7cc62e20380af86d103
Francisco Leví <francisco.levi.md@gmail.com>
2021-03-17 08:50:29 -0600

    Order positives, keep negatives.

--- challenges/random_challenges.py
+++ challenges/random_challenges.py
@@ -1,3 +1,16 @@
+def sort_only_natural_numbers(numbers):
+	"""Sort only the natural numbers within a list. The remaining numbers will remain in their original position
+
+	Args:
+		numbers (list): Original number list.
+
+	Returns:
+		list: The list with the natural numbers ordered.
+	"""
+	natural_numbers = sorted([number for number in numbers if number > 0], reverse=True)
+	return [natural_numbers.pop() if number > 0 else number for number in numbers]
+
+
 def is_palindorme(word):
 	"""Valid if a word is a palindrome.
 
@@ -13,11 +26,16 @@ def is_palindorme(word):
 
 def main():
 	# word = 'Tania'
-	word = 'Reconocer'
-	if is_palindorme(word):
-		print(f'{word} is a palindrome.')
-	else:
-		print(f'{word} isn\'t a palindrome.')
+	# word = 'Reconocer'
+	# if is_palindorme(word):
+	# 	print(f'{word} is a palindrome.')
+	# else:
+	# 	print(f'{word} isn\'t a palindrome.')
+
+	numbers = None	
+	sort_numbers = sort_only_natural_numbers(numbers)
+	print(f'Original list: {numbers}')
+	print(f'Sort list: {sort_numbers}')
 
 
 if __name__ == '__main__':
