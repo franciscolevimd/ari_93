d6b445d213c5074a6d3cca15b8acd84133c5371f
Francisco Leví <francisco.levi.md@gmail.com>
2021-03-17 10:00:02 -0600

    Accumulate challenge.

--- challenges/random_challenges.py
+++ challenges/random_challenges.py
@@ -1,3 +1,34 @@
+from itertools import accumulate
+
+
+def add_accumulated(numbers):
+	"""Returns the add accumulate of each element of a list numbers.
+
+	Example: 
+	
+		In:[1, 5, 7]
+		Out: [1, 6, 13]
+
+	Args:
+		numbers (list): Number list.
+
+	Returns:
+		list: Cumulative sum list.
+	"""
+	# Solution 1
+	# result = []
+	# accumulated = 0
+	# for number in numbers:
+	# 	accumulated += number
+	# 	result.append(accumulated)
+	# return result
+	# Solution 2
+	# return [sum(numbers[:i + 1]) for i in range(len(numbers))]
+	# Solution 3
+	return list(accumulate(numbers))
+
+
+
 def sort_only_natural_numbers(numbers):
 	"""Sort only the natural numbers within a list. The remaining numbers will remain in their original position
 
@@ -32,10 +63,17 @@ def main():
 	# else:
 	# 	print(f'{word} isn\'t a palindrome.')
 
-	numbers = None	
-	sort_numbers = sort_only_natural_numbers(numbers)
-	print(f'Original list: {numbers}')
-	print(f'Sort list: {sort_numbers}')
+	# numbers = None	
+	# sort_numbers = sort_only_natural_numbers(numbers)
+	# print(f'Original list: {numbers}')
+	# print(f'Sort list: {sort_numbers}')
+
+	# numbers = None
+	numbers = []
+	# numbers = [1, 5, 7]
+	accumulate = add_accumulated(numbers)
+	print(f'Numbers: {numbers}')
+	print(f'Accumulate: {accumulate}')
 
 
 if __name__ == '__main__':
